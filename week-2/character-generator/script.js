1c66e38424e62001cc513295687592021da4c587
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-11-03 20:21:32 -0500

    Week 2 assignment commit

--- week-2/character-generator/script.js
+++ week-2/character-generator/script.js
@@ -11,15 +11,34 @@
 "use strict";
 
 function createCharacter(name, gender, characterClass) {
-  // TODO: Implement this function
+  return {
+      getName: function() {
+          return name;
+      },
+      getGender: function() {
+          return gender;
+      },
+      getClass: function() {
+          return characterClass;
+      }
+  };
 }
 
 document.getElementById("generateHero").addEventListener("click", function(e) {
   e.preventDefault();
 
-  // TODO: Get form values
+  const name = document.getElementById("heroName").value;
+  const gender = document.getElementById("heroGender").value;
+  const characterClass = document.getElementById("heroClass").value;
 
   // TODO: Create character
+  const newCharacter = createCharacter(name, gender, characterClass);
 
-  // TODO: Display character information
+  document.getElementById("characterOutput").style.display = "inline-block";
+
+  document.getElementById("characterOutput").innerHTML = `
+  Name: ${newCharacter.getName()}
+  Gender: ${newCharacter.getGender()}
+  Class: ${newCharacter.getClass()}
+`;
 });
