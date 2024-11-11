8d9a099bf7a02fa3ca0481a56a6408e667124eb9
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-11-10 19:13:58 -0500

    Week 3 assignment commit

--- week-3/project09-02/project09-02b.js
+++ week-3/project09-02/project09-02b.js
@@ -4,8 +4,8 @@
       Project 09-01
 
       Project to read field values from session storage
-      Author: 
-      Date:   
+      Author: Sara George
+      Date: 11/09/2024
 
       Filename: project09-02b.js
 */
@@ -20,5 +20,14 @@ let accOption = document.getElementById("accOption");
 let region = document.getElementById("region");
 let miles = document.getElementById("miles");
 let comments = document.getElementById("comments");
+
+riderName.textContent = sessionStorage.getItem("riderName");
+ageGroup.textContent = sessionStorage.getItem("ageGroup");
+bikeOption.textContent = sessionStorage.getItem("bikeOption");
+routeOption.textContent = sessionStorage.getItem("routeOption");
+accOption.textContent = sessionStorage.getItem("accOption");
+region.textContent = sessionStorage.getItem("region");
+miles.textContent = sessionStorage.getItem("miles");
+comments.textContent = sessionStorage.getItem("comments");
 
 
