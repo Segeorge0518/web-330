11096d4a2adfc4f15850e6a2e568d0482c829886
Segeorge0518 <segeorge0518@sbcglobal.net>
2024-11-24 20:50:10 -0500

    Week 5 Assignment commit

--- week-5/restaurant-reservation/script.js
+++ week-5/restaurant-reservation/script.js
@@ -3,24 +3,37 @@
   Chapter 2
   Programming Assignment
 
-  Author:
-  Date:
-  Filename:
+    Author: Sara George
+    Date: 11/22/2024
+    Filename: index.html
 */
 
 // Create an in-memory object array for each table in the restaurant
 let tables = [
-  // Add your table objects here
+  { tableNumber: 1, capacity: 4, isReserved: false },
+  { tableNumber: 2, capacity: 2, isReserved: false },
+  { tableNumber: 3, capacity: 6, isReserved: false },
+  { tableNumber: 4, capacity: 8, isReserved: false },
 ];
 
 // Create a function reserveTable
 function reserveTable(tableNumber, callback, time) {
-  // Add your code here
+  const table = tables.find(t => t.tableNumber === tableNumber);
+  if (table && !table.isReserved) {
+      table.isReserved = true;
+      setTimeout(() => {
+          callback(`Table ${tableNumber} has been reserved.`);
+      }, time);
+  } else {
+      callback(`Error: Table ${tableNumber} is not available.`);
+  }
 }
 
 // When the form is submitted, call the reserveTable function
-document
-  .getElementById("reservationForm")
-  .addEventListener("submit", function (e) {
-    // Add your code here
+document.getElementById("reservationForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const tableNumber = parseInt(document.getElementById('tableNumber').value);
+  reserveTable(tableNumber, function(message) {
+    document.getElementById('message').innerText = message;
+  }, 15000);
   });
